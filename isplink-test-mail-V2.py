2e56b91b04848e6589a0e7426b5a518dc46f1aac
Praveen <49310101+Praveensreedhar@users.noreply.github.com>
2023-09-18 08:30:22 +0530

    Create isplink-test-mail-V2.py

---
+++ isplink-test-mail-V2.py
@@ -0,0 +1,129 @@
+import json
+import time
+import os
+#from influxdb import InfluxDBClient
+#import influxdb
+import imaplib
+import email
+from bs4 import BeautifulSoup
+import smtplib
+from netmiko import ConnectHandler
+from email.mime.text import MIMEText
+
+#os.chdi("C:\\users\\user\\Desktop\\SNMP-FLUS-PY_GRA")
+os.chdir("C:\\Users\\AXTON\\Desktop\\ISP-Link-Test")
+#os.chdir("/AUTOMATION/LINK-TEST/")
+with open('devicestest.json') as dev_file:
+	devices = json.load(dev_file)
+
+for device in devices:
+
+    def snmpget(oid):
+    
+        from pysnmp.entity.rfc3413.oneliner import cmdgen
+    
+        global SNMP_HOST
+        global SNMP_PORT
+        global SNMP_COMMUNITY
+
+        SNMP_HOST = device['ip']
+        SNMP_PORT = 161
+        SNMP_COMMUNITY = 'allyours'
+    
+        cmdGen = cmdgen.CommandGenerator()
+
+        errorIndication, errorStatus, errorIndex, varBinds = cmdGen.getCmd(
+            cmdgen.CommunityData(SNMP_COMMUNITY),
+            cmdgen.UdpTransportTarget((SNMP_HOST, SNMP_PORT)),
+            oid
+        )
+    
+    # Check for errors and print out results
+        if errorIndication:
+            print(errorIndication)
+        else:
+            if errorStatus:
+                print('%s at %s' % (
+                    errorStatus.prettyPrint(),
+                    errorIndex and varBinds[int(errorIndex)-1] or '?'
+                    )
+                )
+            else:
+                for name, val in varBinds:
+                    #print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+                    return val
+    download = snmpget(device['oidd'])
+    upload = snmpget(device['oidu'])
+    hostname = snmpget(device['oidhostname'])
+    rpm = snmpget(device['rpm'])
+    print(hostname)
+    #print(download)
+    #print(upload)
+    print(rpm)
+    print(device['ip'])
+    #print(device['oidd'])
+    ip_value = device['ip']
+    print(ip_value)
+    if rpm > 0:
+        device = {
+            
+            'device_type': 'juniper',
+
+            'ip': ip_value,   # Replace with the IP address of your Cisco device
+
+            'username': 'name',
+
+            'password': 'cisco123',
+
+                }
+        try:
+
+            net_connect = ConnectHandler(**device)
+        except Exception as e:
+            print(f"Failed to connect to the device: {e}")
+            exit()
+
+        try:
+
+            ping_result = net_connect.send_command("ping 10.1.1.2 count 5")
+            print(ping_result)
+            time.sleep(10)
+            print(net_connect.session_log) 
+            if "100% packet loss" in ping_result:
+           	    
+
+                print("Ping failed. No response.")
+                print("Please raise a docket with ISP")
+
+                replay_subject = "TATA 10 MB link Down CKTid:123987"
+                reply_message = """
+		Hello,
+			Please raise a docket with ISP
+		Thanks
+		Praveen S
+		"""                    
+                smtp_server = "smtp.gmail.com"
+                smtp_port = 587
+                smtp_username = "xyza@gmail.com"
+                smtp_password = "123z"
+                sender_email = 'zsc@mail.com, cde@mail.com'
+                with smtplib.SMTP(smtp_server, smtp_port) as server:
+                    server.starttls()
+                    server.login(smtp_username, smtp_password)
+                    msg = MIMEText(reply_message)
+                    msg['Subject'] = replay_subject
+                    msg['From'] = smtp_username
+                    msg['To'] = sender_email
+                    server.sendmail(smtp_username, sender_email, msg.as_string())
+                    print("Mail sent successfully.")
+
+        except Exception as e:
+                
+                print(f"Error executing the ping command: ")
+        net_connect.disconnect()
+
+    #v = 2000
+    #vv = 6000
+    #hostname = "TRV-TATA"
+    else:
+       print("TRV-MAA-IPLC CKTid:123987 is UP now  ")
